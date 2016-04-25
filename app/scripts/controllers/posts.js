4f37582d87c6829c01c11e867c1f1b06bb0a9a37
bugyt <admin@bug.yt>
2016-04-25 12:25:53 +0200

    switch to Marionette

---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function() {
+   app.Controllers.Posts = Marionette.Object.extend({
+  //   initialize: function(options) {
+  //     // console.log(options.name);
+  //   },
+  //   graduate: function() {
+  //     this.triggerMethod('announce', 'I graduated!!!');
+  //   }
+  });
+
+})();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,37 @@
 'use strict';
-var log = console.log.bind(console);
-var app = {
+
+var App = Marionette.Application.extend({
   Models: {},
   Collections: {},
   Views: {},
   Routers: {},
-  Helpers: {},
-  init: function() {
+  Controllers: {},
+  Helpers: {}
+});
+
+Marionette.Application.prototype.Models = {};
+Marionette.Application.prototype.Collections = {};
+Marionette.Application.prototype.Views = {};
+Marionette.Application.prototype.Routers = {};
+Marionette.Application.prototype.Controllers = {};
+Marionette.Application.prototype.Helpers = {};
+
+var app = new Marionette.Application();
+
+$(function() {
+  app.on('start', function() {
+    marked.setOptions({
+      highlight: function(code) {
+        return hljs.highlightAuto(code).value;
+      }
+    });
+    window.onerror = function(error) {
+      alert(error);
+    };
     app.mainView = app.mainView || new app.Views.App();
+    app.postController = app.postController || new app.Controllers.Posts();
     app.mainRouter = app.mainRouter || new app.Routers();
     Backbone.history.start();
-  }
-};
-
-window.onerror = function(error) {
-  alert(error);
-};
-log('test');
-$(function() {
-  marked.setOptions({
-    highlight: function(code) {
-      return hljs.highlightAuto(code).value;
-    }
   });
-  app.init();
+  app.start();
 });
